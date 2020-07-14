f55a8a378b3778a47ddf444427910cd2806d9f7b
leesangha <37141545+leesangha@users.noreply.github.com>
2020-07-14 20:33:01 +0900

    first commit

---
+++ styling-with-sass/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "./components/Button";
+function App() {
+  return (
+    <div className="App">
+      <div className="buttons">
+        <Button>Buttons</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
