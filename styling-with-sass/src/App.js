43c6e6c3aa44291d901263d67c34e560e142ca9a
leesangha <37141545+leesangha@users.noreply.github.com>
2020-07-15 20:02:14 +0900

    Update App.js

--- styling-with-sass/src/App.js
+++ styling-with-sass/src/App.js
@@ -1,10 +1,41 @@
 import React from "react";
 import Button from "./components/Button";
+import "./App.scss";
 function App() {
   return (
     <div className="App">
       <div className="buttons">
-        <Button>Buttons</Button>
+        <Button color="blue" size="large">
+          Buttons
+        </Button>
+        <Button color="blue" size="medium">
+          Buttons
+        </Button>
+        <Button color="blue" size="small">
+          Buttons
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button color="gray" size="large">
+          Buttons
+        </Button>
+        <Button color="gray" size="medium">
+          Buttons
+        </Button>
+        <Button color="gray" size="small">
+          Buttons
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button color="pink" size="large">
+          Buttons
+        </Button>
+        <Button color="pink" size="medium">
+          Buttons
+        </Button>
+        <Button color="pink" size="small">
+          Buttons
+        </Button>
       </div>
     </div>
   );
