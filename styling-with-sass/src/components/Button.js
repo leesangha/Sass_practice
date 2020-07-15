3cf04ad466f2916ed6d9c2275777108b79738e09
leesangha <37141545+leesangha@users.noreply.github.com>
2020-07-15 20:02:20 +0900

    Update Button.js

--- styling-with-sass/src/components/Button.js
+++ styling-with-sass/src/components/Button.js
@@ -1,8 +1,15 @@
 import React from "react";
+import classNames from "classnames";
 import "./Button.scss";
 
-function Button({ children }) {
-  return <button className="Button"> {children}</button>;
+function Button({ children, size, color }) {
+  return (
+    <button className={classNames("Button", size, color)}> {children}</button>
+  );
 }
 
+Button.defaultProps = {
+  size: "medium",
+  color: "blue",
+};
 export default Button;
