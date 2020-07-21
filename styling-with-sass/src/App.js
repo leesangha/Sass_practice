f11de2f83e3f15a2dada8a882c5181a7ce318b6f
leesangha <sanghalee1@naver.com>
2020-07-21 14:05:25 +0900

    Update App.js

--- styling-with-sass/src/App.js
+++ styling-with-sass/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Button from "./components/Button";
 import "./App.scss";
 function App() {
-  const asdf = 'asdf'
+  const asdf = "asdf";
   return (
     <div className="App">
       <div className="buttons">
