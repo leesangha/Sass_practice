859fb309f424e5534df188f1c6bfa9fe674fe923
leesangha <sanghalee1@naver.com>
2020-07-22 17:14:21 +0900

    modify

--- styling-with-styled-components/src/App.js
+++ styling-with-styled-components/src/App.js
@@ -9,6 +9,11 @@ const AppBlock = styled.div`
   padding: 1rem;
 `;
 
+const ButtonGroup = styled.div`
+  & + & {
+    margin-top: 1rem;
+  }
+`;
 const palette = {
   blue: '#228be6',
   gray: '#496057',
@@ -18,7 +23,33 @@ function App() {
   return (
     <ThemeProvider theme={{ palette }}>
       <AppBlock>
-        <Button>Button</Button>
+        <ButtonGroup>
+          <Button color="gray" size="large">
+            Button
+          </Button>
+          <Button color="gray">Button </Button>
+          <Button color="gray" size="small">
+            Button
+          </Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button color="blue" size="large">
+            Button
+          </Button>
+          <Button color="blue">Button </Button>
+          <Button color="blue" size="small">
+            Button
+          </Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button color="pink" size="large">
+            Button
+          </Button>
+          <Button color="pink">Button </Button>
+          <Button color="pink" size="small">
+            Button
+          </Button>
+        </ButtonGroup>
       </AppBlock>
     </ThemeProvider>
   );
--- styling-with-styled-components/src/components/Button.js
+++ styling-with-styled-components/src/components/Button.js
@@ -1,6 +1,41 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { darken, lighten } from 'polished';
+const colorStyles = css`
+  /*색상*/
+  ${({ theme, color }) => {
+    const selected = theme.palette[color];
+    return css`
+      background: ${selected};
+      &:hover {
+        background: ${lighten(0.1, selected)};
+      }
+      &:active {
+        background: ${darken(0.1, selected)};
+      }
+    `;
+  }}
+`;
+const sizeStyles = css`
+${(props) =>
+  props.size === 'large' &&
+  css`
+    height: 3rem;
+    font-size: 1.25rem;
+  `}
+    ${(props) =>
+      props.size === 'medium' &&
+      css`
+        height: 2rem;
+        font-size: 1rem;
+      `}
+    ${(props) =>
+      props.size === 'small' &&
+      css`
+        height: 1.75rem;
+        font-size: 0.8rem;
+      `}`;
+
 const StyledButton = styled.button`
   /*공통 스타일 */
   display: inline-flex;
@@ -15,24 +50,37 @@ const StyledButton = styled.button`
   align-items: center;
 
   /*크기 */
-  height: 2.25rem;
-  font-size: 1rem;
-  /*색상*/
-  background: #228b26;
-  &:hover {
-    background: ${lighten(0.1, '#228be6')};
-  }
-  &:active {
-    /*클릭시 */
-    background: ${darken(0.1, '#228be6')};
-  }
+  ${sizeStyles}
+  ${colorStyles}
   /*기타 */
   & + & {
     margin-left: 1rem;
   }
 `;
-function Button({ children, ...rest }) {
-  return <StyledButton {...rest}>{children}</StyledButton>;
-}
 
+const sizes = {
+  large: {
+    height: '3rem',
+    fontSize: '1.25rem',
+  },
+  medium: {
+    height: '2.25rem',
+    fontSize: '1rem',
+  },
+  small: {
+    height: '1.5rem',
+    fontSize: '0.8rem',
+  },
+};
+function Button({ children, color, size, ...rest }) {
+  return (
+    <StyledButton color={color} size={size} {...rest}>
+      {children}
+    </StyledButton>
+  );
+}
+Button.defaultProps = {
+  color: 'blue',
+  size: 'medium',
+};
 export default Button;
