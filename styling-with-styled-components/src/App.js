857a853eae256b043ef50e4517fb6066142e825a
leesangha <sanghalee1@naver.com>
2020-07-23 14:38:48 +0900

    Update App.js

--- styling-with-styled-components/src/App.js
+++ styling-with-styled-components/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import Button from './components/Button';
 import Dialog from './components/Dialog';
@@ -21,27 +21,31 @@ const palette = {
   pink: '#f06595',
 };
 function App() {
+  const [dialog, setDialog] = useState(false);
+  const onClick = () => {
+    setDialog(true);
+  };
+  const onConfirm = () => {
+    setDialog(false);
+    console.log('확인');
+  };
+  const onCancel = () => {
+    setDialog(false);
+    console.log('취소');
+  };
   return (
     <ThemeProvider theme={{ palette }}>
       <AppBlock>
-        <ButtonGroup>
-          <Button color="gray" size="large" outline>
-            Button
-          </Button>
-          <Button color="gray">Button </Button>
-          <Button color="gray" size="small">
-            Button
-          </Button>
-        </ButtonGroup>
-        <ButtonGroup>
-          <Button color="blue" size="large" fullWidth>
-            Button
-          </Button>
-        </ButtonGroup>
+        <Button color="pink" size="large" onClick={onClick}>
+          삭제
+        </Button>
         <Dialog
           title="정말로 삭제하시겠습니까?"
           confirmText="삭제"
           cancelText="취소"
+          visible={dialog}
+          onConfirm={onConfirm}
+          onCancel={onCancel}
         >
           데이터를 정말로 삭제하시겠습니까?
         </Dialog>
